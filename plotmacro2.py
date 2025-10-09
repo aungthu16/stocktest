dcaed06916d521db04891505ab8549b828997ad9
aungthu16 <aprilboy.new@gmail.com>
2025-10-09 20:45:12 +0700

    Update plotmacro2.py

--- plotmacro2.py
+++ plotmacro2.py
@@ -14,7 +14,7 @@ import re
 
 st.set_page_config(layout="wide", page_title="Economic Analysis Dashboard", page_icon="./Image/logo.png")
 
-ai_model = 'qwen/qwen3-32b'
+ai_model = 'meta-llama/llama-prompt-guard-2-86m'
 FRED_API_KEY = st.secrets["FRED_API_KEY"]
 NUM_POINTS = 20
 GROWTH_COLOR = 'red'
